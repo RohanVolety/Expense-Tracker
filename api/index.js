fb77d74055ad480f75f5620b6d90c086dfdb9994
Solaris <91217295+RohanVolety@users.noreply.github.com>
2023-03-23 22:47:31 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,33 +1,47 @@
-const express=require('express');
-const app=express();
-const cors=require('cors');
+const express = require('express');
+const app = express();
+const cors = require('cors');
 require('dotenv').config();
-const Transaction=require('./models/Transaction.js')
-const mongoose=require('mongoose');
+const Transaction = require('./models/Transaction')
+const mongoose = require('mongoose');
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/test',(req,res)=>{
-
-app.use(express.json())
-res.json('test ok');
+app.get('/api/test', (req, res) => {
+    app.use(express.json())
+    res.json('test ok');
 });
 
-app.post('/api/transaction',async (req,res)=>{
-   //console.log(process.env.MONGO_URL);
-    await mongoose.connect(process.env.MONGO_URL);
-    const {name,price,description,datetime}=req.body;
-    const trans=new trans({ name,price,description,datetime })
-    const transaction= await Transaction.create(trans);
-   
-res.json(transaction);
+app.post('/api/transaction', async (req, res) => {
+    try {
+        const { name, price, description, datetime } = req.body;
+        const newtrans = await Transaction.create({ name, price, description, datetime });
+        
+        res.status(201).json({ newtrans, "success": true })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 });
 
-app.get('/api/transactions',async (req,res)=>{  
-     await mongoose.connect(process.env.MONGO_URL);
-    const transactions=await Transaction.find();
-    res.json(transactions);
+app.get('/api/transactions', async (req, res) => {
+    try {
+        console.log('get transactions')
+        const transactions = await Transaction.find();
+        res.json(transactions);
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
 });
 
-app.listen(4000,()=>console.log('server started on port 4000'));
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        app.listen(4000, () => console.log('server started on port 4000'));
+    })
+    .catch(err => console.log(err));
+
+
